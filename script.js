a6fa1a71841907601fffc8daa6d90bcf9279421c
Mouad Naji <mouadnaji@Mouads-MacBook-Pro.local>
2019-04-23 13:06:08 +0200

    eksempel

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", hentJson);
+
+
+
+
+//For geomuseum faxe
 let indhold;
 async function hentJson() {
-    const url = "http://sunnivahyllseth.com/kea/cms/wordpress/wp-json/wp/v2/pages/10";
+    const url = "http://sunnivahyllseth.com/kea/cms/wordpress//wp-json/wp/v2/Museumer/160";
     //henter data filen
     const myJson = await fetch(url);
     //den hentede data skal tolkes som json
@@ -11,6 +16,21 @@ async function hentJson() {
 };
 
 function visIndhold() {
-    document.querySelector(".eksempel").textContent = indhold.title.rendered;
-    document.querySelector(".eksempel2").innerHTML = indhold.content.rendered;
+    document.querySelector(".eksempel11").innerHTML += indhold.museum_overskrift;
+
+    document.querySelector(".eksempel11").innerHTML += `<img class="SplashBillede" src=${indhold.splashbillede.guid} alt="billede">`;
+
+    document.querySelector(".eksempel11").innerHTML += indhold.titeloverskrift;
+    document.querySelector(".eksempel11").innerHTML += indhold.tekst;
+
+    document.querySelector(".eksempel11").innerHTML += `<img class="Billeder" src=${indhold.billeder.guid} alt="billede">`;
+
+    document.querySelector(".eksempel11").innerHTML += indhold.tekst_2;
+
+
+
+
+
+
+
 }
